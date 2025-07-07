e8fbfdf97394c70cdac5c1307240fd80e57fcc8a
FrankBonanno <fb0023@mix.wvu.edu>
2025-07-06 23:36:09 -0400

    delete feed follows

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/FrankBonanno/go-web-scraper/internal/database"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 )
 
@@ -46,5 +47,25 @@ func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	respondWithJson(w, 201, databaseFeedFollowsToFeedFollows(feedFollows))
+	respondWithJson(w, 200, databaseFeedFollowsToFeedFollows(feedFollows))
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollowIdStr := chi.URLParam(r, "feedFollowId")
+	feedFollowId, err := uuid.Parse(feedFollowIdStr)
+	if err != nil {
+		resondWithError(w, 400, fmt.Sprintf("Couldn't parse feed follow id: %s", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
+		ID: feedFollowId,
+		UserID: user.ID,
+	})
+	if err != nil {
+		resondWithError(w, 400, fmt.Sprintf("Couldn't delete feed follow: %s", err))
+		return
+	}
+
+	respondWithJson(w, 200, struct{}{})
 }
